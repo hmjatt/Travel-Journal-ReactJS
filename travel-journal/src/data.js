d15b7d9e96b40c7d4d71b629edf697aa009086a2
Harmeet Matharoo <hmjatt@gmail.com>
2022-08-02 22:13:55 -0600

    Added `Opera House` card

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -61,13 +61,13 @@ const data = [
     },
 	{
 		id: 7,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: require("./images/mt-fuji.jpg")
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.213108,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
+        startDate: "27 May, 2025",
+        endDate: "8 Jun, 2025",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: require("./images/opera-house.jpg")
     }
 ];
 
